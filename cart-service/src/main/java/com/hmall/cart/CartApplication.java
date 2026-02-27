1f587fd9d625c646f7861aae841f598204e9fe99
Ballauma <254620450+Ballauma@users.noreply.github.com>
2026-02-27 15:57:40 +0800

    添加nacos，openfeign和okhttp

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -3,12 +3,14 @@ package com.hmall.cart;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * @author Ballauma
  */
+@EnableFeignClients
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
