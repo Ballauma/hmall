6744df421c244806c9b3a0e58e049e9f39451d03
Ballauma <254620450+Ballauma@users.noreply.github.com>
2026-03-15 14:45:05 +0800

    为支付模块和交易模块添加Mq消息队列

---
+++ hm-common/src/main/java/com/hmall/common/config/MqConfig.java
@@ -0,0 +1,15 @@
+package com.hmall.common.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Ballauma
+ */
+@Configuration
+public class MqConfig {
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+}
